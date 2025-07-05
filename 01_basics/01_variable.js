8101d2dbf7ac5db0344a8a26a44deca96985217c
piyushtiwari30 <tiwaripiyush533@gmail.com>
2025-07-05 20:28:47 +0000

    variable learnig

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accoutnId=23325;
+let accountEmail="hitesh@google.com";
+var accoutPass="234233";
+accountCity="jaipur";
+console.table([accoutnId,accountEmail,accoutPass,accountCity]);
+//accoutnId=23523;
+// console.log(accoutnId);
+// it is not a good practice to use the var keyword
+// due to the scope problem
+accountEmail="hk@hk.com";
+accoutPass=1212120;//we can change the datatype here too
+accountCity="bangluru";
+let accountState;
+console.table([accoutnId,accountEmail,accoutPass,accountCity,accountState]);
