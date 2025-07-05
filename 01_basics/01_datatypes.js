7816b8c746cd9aee3fb2d36d403789219cce68a2
piyushtiwari30 <tiwaripiyush533@gmail.com>
2025-07-05 21:56:54 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"//treat js code as newer version 
+//alert(3+3)//we are useing node js, not browser so this will not give output
+console.log(3
+    +3
+);//eventhough it gave the output but the readeablity always come first
+console.log("piyush");
+
+let name="piyush";
+let age=45;
+let procedure=true;
+let state;//this will give undefine
+
+//number=>2 to power 53
+// bigint
+//string=> ""
+//boolen=> true / false
+//null=> stand alone value
+//undefine=>
+//symbole=> to define uniqeness in the react js
+
+//object
+
+console.log(typeof "piyush");//string
+console.log(typeof null);//this will give object
+console.log(typeof undefined);//this will give undfined 
+
+
+
