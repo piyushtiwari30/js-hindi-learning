4b1362d30756ef6d2c53c102e0301dcd7d468ee7
piyushtiwari30 <tiwaripiyush533@gmail.com>
2025-07-06 19:53:12 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,17 @@
+let age=33;
+let score="33abc";
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+// "33"=>33
+// true=>1;false=>0;
+// "33abc"=>nan;
+//in type of valueInNumber it is giving the number but when printing the "valueInNumber" in console it is giving the above things on console.log
+//1=>true;0=>false;
+//""=>;false
+//"piyush"=>true;
+let AValue=1;
+let BooleanAValue=Boolean(AValue);
+console.log(BooleanAValue);
