db685a821741ac11c9fdd5918b46da7e3dbe1ece
piyushtiwari30 <tiwaripiyush533@gmail.com>
2025-07-06 21:20:19 +0000

    adding the stack and heap

--- 01_basics/09_datatypes.js
+++ 01_basics/09_datatypes.js
@@ -1,4 +1,5 @@
 //primitive
+
 //7types:string,numbers,null,undefine,boolean,BigInt,symbol
 const Id=Symbol('123');
 const AnotherId=Symbol('123');
@@ -31,3 +32,28 @@ console.log(Id===AnotherId);
 //number=>number;
 //symbol=>symbol;
 //Boolean=>Boolean;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack(primitive) and heap(non-primitive)
+let myname="hitesh";
+let OtherName=myname;
+//there when we will print both will give same value
+OtherNmae="ganesh";
+//now if we wil print both the value of myname will remain same but the OtheName has been changed to ganesh , as the value being passed here is a copy in stack;
+
+
+//but in the non-primitive type the value is passed by refence, so the change in one will be reflected in other
+
+let userone={
+    name:"hitesh",
+    email:"hites@bl"
+}
+let usertwo=userone;
+console.log(userone.email)
+console.log(usertwo.email)
+
+usertwo.email="gil@bl"
+
+console.log(userone.email)
+console.log(usertwo.email)
+
